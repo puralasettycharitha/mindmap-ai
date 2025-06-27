744ef89c226c657803ab04bec5cc4d289b423c14
puralasettycharitha <charithap2004@gmail.com>
2025-06-27 09:37:12 +0530

    Fix: auto-download spaCy model on app start

--- mindmap_generator.py
+++ mindmap_generator.py
@@ -4,7 +4,16 @@ import spacy
 import networkx as nx
 
 import spacy
-nlp = spacy.load("en_core_web_sm")
+import spacy
+import subprocess
+import sys
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
+
 
 
 def build_mindmap(text):
