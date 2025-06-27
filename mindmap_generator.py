b256b1fa360978ecafe1b88ef7c9a1855d007aa1
puralasettycharitha <charithap2004@gmail.com>
2025-06-27 09:30:28 +0530

    Fix build: use spacy model from render.yaml

--- mindmap_generator.py
+++ mindmap_generator.py
@@ -3,8 +3,9 @@
 import spacy
 import networkx as nx
 
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
 
 def build_mindmap(text):
     doc = nlp(text)
