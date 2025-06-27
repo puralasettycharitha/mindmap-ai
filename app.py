150b1c2f66e53f7b0d397e6c6740dbe8ec2fac44
puralasettycharitha <charithap2004@gmail.com>
2025-06-27 10:03:38 +0530

    Fix: bind Dash to 0.0.0.0 and Render port

--- app.py
+++ app.py
@@ -208,4 +208,4 @@ def upload_json(contents):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run_server(debug=False, host='0.0.0.0', port=port)
